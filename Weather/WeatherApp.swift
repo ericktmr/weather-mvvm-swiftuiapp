46dc2f3e0b84c8dacb4ffba8cada08279ded7d0c
Wilker Oliveira <wilkerso@tribanco.com.br>
2021-08-14 21:51:04 -0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Tribanco on 14/08/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
